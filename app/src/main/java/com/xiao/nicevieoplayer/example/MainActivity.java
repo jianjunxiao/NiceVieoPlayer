ee29737e24179b68a7b42206fd3c9e6a5b366298
xiaojianjun <342164553@qq.com>
2017-06-20 09:56:53 +0800

    1.修改播放完或播放错误时重新播放的逻辑 2.在小窗口可以直接进入全屏 3.按下Home键时暂停播放，重新进入继续播放 4.全屏或小窗口播放完毕不自动回到Normal状态

--- app/src/main/java/com/xiao/nicevieoplayer/example/MainActivity.java
+++ app/src/main/java/com/xiao/nicevieoplayer/example/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void init() {
         mNiceVideoPlayer = (NiceVideoPlayer) findViewById(R.id.nice_video_player);
+        // developer
         mNiceVideoPlayer.setPlayerType(NiceVideoPlayer.PLAYER_TYPE_IJK); // IjkPlayer or MediaPlayer
         mNiceVideoPlayer.setUp("http://tanzi27niu.cdsb.mobi/wps/wp-content/uploads/2017/05/2017-05-17_17-33-30.mp4", null);
         NiceVideoPlayerController controller = new NiceVideoPlayerController(this);
